879d2c38f6803ac2521bd5a24b1cbb1a6bc006f9
stonedcloud <134713973+stonedcloud@users.noreply.github.com>
2023-05-26 17:23:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 setup(name='tinygrad',
       version='0.6.0',
       description='You like pytorch? You like micrograd? You love tinygrad! <3',
-      author='George Hotz',
-      license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
       packages = ['tinygrad', 'tinygrad.codegen', 'tinygrad.nn', 'tinygrad.runtime', 'tinygrad.shape'],
@@ -23,7 +21,6 @@ setup(name='tinygrad',
       python_requires='>=3.8',
       extras_require={
         'llvm': ["llvmlite"],
-        'cuda': ["pycuda"],
         'triton': ["triton>=2.0.0.dev20221202"],
         'metal': ["pyobjc-framework-Metal", "pyobjc-framework-Cocoa", "pyobjc-framework-libdispatch"],
         'linting': [
